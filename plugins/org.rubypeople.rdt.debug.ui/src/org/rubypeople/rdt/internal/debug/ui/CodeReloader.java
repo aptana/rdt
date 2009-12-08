3d16900a1bb0ddf771bca54077768bddbcc09f01
sgtcoolguy <cwilliams@aptana.com>
2009-12-08 14:44:06 -0800

    register self as listener in a UIJob that gets shceduled rather than in main thread so that it can run when it's ready and hopefully not slow down startup of plugin which results in the ugly "deadlock" avoidance message of Eclipse

--- plugins/org.rubypeople.rdt.debug.ui/src/org/rubypeople/rdt/internal/debug/ui/CodeReloader.java
+++ plugins/org.rubypeople.rdt.debug.ui/src/org/rubypeople/rdt/internal/debug/ui/CodeReloader.java
@@ -12,12 +12,13 @@ package org.rubypeople.rdt.internal.debug.ui;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchManager;
-import org.eclipse.debug.internal.ui.DebugUIPlugin;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IPartListener;
 import org.eclipse.ui.IPropertyListener;
@@ -25,102 +26,155 @@ import org.eclipse.ui.IWindowListener;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.progress.UIJob;
 import org.rubypeople.rdt.internal.debug.core.model.IRubyDebugTarget;
 import org.rubypeople.rdt.internal.ui.rubyeditor.RubyEditor;
 import org.rubypeople.rdt.launching.IRubyLaunchConfigurationConstants;
 
-public class CodeReloader implements IPartListener, IPropertyListener, IWindowListener {
+public class CodeReloader implements IPartListener, IPropertyListener, IWindowListener
+{
 
-	public void windowActivated(IWorkbenchWindow window) {}
+	public void windowActivated(IWorkbenchWindow window)
+	{
+	}
 
-	public void windowClosed(IWorkbenchWindow window) {
+	public void windowClosed(IWorkbenchWindow window)
+	{
 		window.getPartService().removePartListener(this);
 	}
 
-	public void windowDeactivated(IWorkbenchWindow window) {}
+	public void windowDeactivated(IWorkbenchWindow window)
+	{
+	}
 
-	public void windowOpened(IWorkbenchWindow window) {
+	public void windowOpened(IWorkbenchWindow window)
+	{
 		window.getPartService().addPartListener(this);
 	}
 
-	public void partActivated(IWorkbenchPart part) {}
+	public void partActivated(IWorkbenchPart part)
+	{
+	}
 
-	public void partBroughtToTop(IWorkbenchPart part) {}
+	public void partBroughtToTop(IWorkbenchPart part)
+	{
+	}
 
-	public void partClosed(IWorkbenchPart part) {
-		if (part instanceof RubyEditor) {
+	public void partClosed(IWorkbenchPart part)
+	{
+		if (part instanceof RubyEditor)
+		{
 			part.removePropertyListener(this);
 		}
 	}
 
-	public void partOpened(IWorkbenchPart part) {
+	public void partOpened(IWorkbenchPart part)
+	{
 		this.addAsListener(part);
 	}
 
-	public void partDeactivated(IWorkbenchPart part) {}
+	public void partDeactivated(IWorkbenchPart part)
+	{
+	}
 
-	public void propertyChanged(Object source, int propId) {
-		if (!(source instanceof IEditorPart)) { return; }
-		if (propId != IEditorPart.PROP_DIRTY) { return; }
+	public void propertyChanged(Object source, int propId)
+	{
+		if (!(source instanceof IEditorPart))
+		{
+			return;
+		}
+		if (propId != IEditorPart.PROP_DIRTY)
+		{
+			return;
+		}
 
 		RubyEditor editor = (RubyEditor) source;
-		if (editor.isDirty()) {
+		if (editor.isDirty())
+		{
 			// editor has not been saved, but code of a previously clean editor
 			// has been edited
 			return;
 		}
 		ILaunch[] launches = DebugPlugin.getDefault().getLaunchManager().getLaunches();
-		for (int i = 0; i < launches.length; i++) {
-			try {
+		for (int i = 0; i < launches.length; i++)
+		{
+			try
+			{
 				ILaunch launch = launches[i];
-				if (!launch.getLaunchMode().equals(ILaunchManager.DEBUG_MODE)) {
+				if (!launch.getLaunchMode().equals(ILaunchManager.DEBUG_MODE))
+				{
 					continue;
 				}
-				if (!launch.getLaunchConfiguration().getType().getIdentifier().equals(IRubyLaunchConfigurationConstants.ID_RUBY_APPLICATION)) {
+				if (!launch.getLaunchConfiguration().getType().getIdentifier().equals(
+						IRubyLaunchConfigurationConstants.ID_RUBY_APPLICATION))
+				{
 					continue;
 				}
 				final IRubyDebugTarget target = (IRubyDebugTarget) launch.getDebugTarget();
-				if (target.isTerminated() || target.isDisconnected()) {
+				if (target.isTerminated() || target.isDisconnected())
+				{
 					continue;
 				}
 				final IResource resource = (IResource) editor.getEditorInput().getAdapter(IResource.class);
-				if (resource == null) {
+				if (resource == null)
+				{
 					continue;
 				}
 				CodeReloadJob job = new CodeReloadJob(target, resource.getLocation().toOSString());
 				job.schedule();
-			} catch (CoreException e) {
-				RdtDebugUiPlugin.log(new Status(IStatus.ERROR, RdtDebugUiPlugin.PLUGIN_ID, 0, "Error while trying to reload file.", e));
+			}
+			catch (CoreException e)
+			{
+				RdtDebugUiPlugin.log(new Status(IStatus.ERROR, RdtDebugUiPlugin.PLUGIN_ID, 0,
+						"Error while trying to reload file.", e));
 			}
 		}
 	}
 
-	public void addAsListener(IWorkbenchPart part) {
-		if (part == null || !(part instanceof RubyEditor)) { return; }
+	public void addAsListener(IWorkbenchPart part)
+	{
+		if (part == null || !(part instanceof RubyEditor))
+		{
+			return;
+		}
 		part.addPropertyListener(this);
 	}
 
-	public CodeReloader() {
-		RdtDebugUiPlugin.getDefault().getWorkbench().addWindowListener(this);
-		IWorkbenchWindow[] windows = RdtDebugUiPlugin.getDefault().getWorkbench().getWorkbenchWindows();
-		for (int i = 0; i < windows.length; i++) {
-			IWorkbenchWindow window = windows[i];
-			window.getPartService().addPartListener(this);
-		}
+	public CodeReloader()
+	{
 		// the CodeReloader constructor is not always called from a UI thread,
 		// e.g. if debug.ui is activated from
 		// pressing F11. If activation is triggered from adding a breakpoint
 		// this is part of a UI thread.
-		DebugUIPlugin.getStandardDisplay().syncExec(new Runnable() {
+		final CodeReloader self = this;
+		UIJob job = new UIJob("")
+		{
 
-			public void run() {
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor)
+			{
+				RdtDebugUiPlugin.getDefault().getWorkbench().addWindowListener(self);
+				IWorkbenchWindow[] windows = RdtDebugUiPlugin.getDefault().getWorkbench().getWorkbenchWindows();
+				for (int i = 0; i < windows.length; i++)
+				{
+					IWorkbenchWindow window = windows[i];
+					window.getPartService().addPartListener(self);
+				}
 				IWorkbenchWindow activeWindow = RdtDebugUiPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow();
-				if (activeWindow == null) { return; }
+				if (activeWindow == null)
+				{
+					return Status.OK_STATUS;
+				}
 				IWorkbenchPage page = activeWindow.getActivePage();
-				if (page == null) { return; }
+				if (page == null)
+				{
+					return Status.OK_STATUS;
+				}
 				addAsListener(page.getActivePart());
+				return Status.OK_STATUS;
 			}
-		});
+		};
+		job.setPriority(Job.LONG);
+		job.schedule();
 	}
 }
-
